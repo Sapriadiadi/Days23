9c101c07dadbf88a5b786676019744bdb87c6959
Sapriadi <147253388+Sapriadiadi@users.noreply.github.com>
2023-10-31 22:11:00 +0800

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,31 @@
+package days23;
+import java.util.Scanner;
+public class Main {
+    
+    public static void main(String[] args) {
+        
+        Scanner input= new Scanner(System.in);
+        String nama,nim,kelas,alamat;
+        System.out.print("masukkan nama : " );
+        nama = input.nextLine ();
+        System.out.print("masukkan nim : " );
+        nim = input.nextLine ();
+        System.out.print("masukkan kelas : " );
+        kelas = input.nextLine ();
+        System.out.print("masukkan alamat : " );
+        alamat=input.nextLine ();
+        System.out.print("masukkan umur : " );
+        int umur= input.nextInt  ();
+        System.out.print("masukkan tinggi badan : " );
+        double tinggi_badan=input.nextDouble();
+        
+        
+        System.out.print("umur : " + nama );
+        System.out.print("nim : " + nim );
+        System.out.print("kelas : " + kelas );
+        System.out.print("alamat : " + alamat );
+        System.out.println("umur : " + umur + "tahun" );
+        System.out.println("tinggi badan :" + tinggi_badan + "cm");
+    }
+    
+}
